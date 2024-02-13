469263497468324afcbc58781af249cf1332c5b4
eunbi-kang <ebzzang1905@naver.com>
2024-02-13 21:49:40 +0900

    profile component 생성 및 기본적인 Stylesheet 적용

---
+++ basic/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className="profile">
+        <img className="photo" src="https://avatars.githubusercontent.com/u/87633110?v=4" 
+        alt='avatar'/>
+        <h1>James Kim</h1>
+        <p>프론트엔드 개발자</p>
+
+    </div>
+  )
+}
