db80ceffb4a86d157314328f722aa767bacf1fb6
Eunbi-Kang <ebzzang1905@naver.com>
2024-02-14 15:04:38 +0900

    신입 개발자 태그 만들기

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Profile({image, name, title}) {
+export default function Profile({image, name, title, isNew }) {
   return (
     <div className="profile">
       <img
@@ -8,6 +8,7 @@ export default function Profile({image, name, title}) {
         src={image}
         alt="avatar"
       />
+      {isNew && <span className="new">New</span>}
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
